808fbf7bea376cf883a46782e761fef5a1839bd6
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2025-05-28 15:59:41 +0200

    Improve Contact Entity

--- src/main/java/com/example/accessingdatajpa/Contact.java
+++ src/main/java/com/example/accessingdatajpa/Contact.java
@@ -1,26 +1,51 @@
 package com.example.accessingdatajpa;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Contact {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
 
-    // Constructeurs
-    public Contact() {}
+    protected Contact() {}
 
-    public Contact(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
+    public Contact(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
     }
 
-    // Getters et Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    @Override
+    public String toString() {
+        return String.format(
+                "Contact[id=%d, firstName='%s', lastName='%s', email='%s']",
+                id, firstName, lastName, email);
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getEmail() { return email; }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() {
+        return email;
+    }
     public void setEmail(String email) { this.email = email; }
 }
